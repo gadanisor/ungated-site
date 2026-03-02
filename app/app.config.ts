b0ca27e2f8d917d7f25f69a0e9d79013f1df31b2
Alexandru Dănișor <gadanisor@gmail.com>
2026-03-02 17:16:24 +0900

    update installation page

--- app/app.config.ts
+++ app/app.config.ts
@@ -3,6 +3,17 @@ export default defineAppConfig({
     colors: {
       primary: 'blue',
       neutral: 'zinc'
+    },
+    prose: {
+      codeIcon: {
+        powershell: 'i-codicon-terminal-powershell',
+        terminal: 'i-codicon-terminal',
+        linux: 'i-codicon-terminal-linux',
+        mac: 'i-codicon-terminal',
+        appimage: 'i-codicon-star-empty',
+        deb: 'i-codicon-terminal-debian',
+        rpm: 'i-codicon-package',
+      }
     }
   }
 })
